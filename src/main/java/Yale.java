b6f13e05e689dd47f1e7b86914e9c237a9903017
DELLXPS13JOSHUA\joshu <joshuagan9@gmail.com>
2025-01-23 12:00:12 +0800

    Added echoing to the chatbot Added indents to chatbot output

--- src/main/java/Yale.java
+++ src/main/java/Yale.java
@@ -1,21 +1,38 @@
+import java.util.Scanner;
+
 public class Yale {
     private static final String NAME = "Yale";
-    private static final String LINE = "____________________________________________________________";
+    private static final String LINE = "\t____________________________________________________________";
 
     public static void main(String[] args) {
         greet();
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String msg = in.nextLine();
+            if (msg.equalsIgnoreCase("bye")) {
+                break;
+            }
+            echo(msg);
+        }
         goodbye();
+    }
+
+    private static void echo(String msg) {
+        System.out.println(LINE);
+        System.out.println("\t" + msg);
+        System.out.println(LINE);
     }
 
     private static void greet() {
         System.out.println(LINE);
-        System.out.printf("Hello! I'm %s.\n", NAME);
-        System.out.println("What can I do for you?");
+        System.out.printf("\tHello! I'm %s.\n", NAME);
+        System.out.println("\tWhat can I do for you?");
         System.out.println(LINE);
     }
 
     private static void goodbye() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+        System.out.println("\tBye. Hope to see you again soon!");
         System.out.println(LINE);
     }
 }
