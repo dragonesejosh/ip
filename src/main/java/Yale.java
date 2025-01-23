2948fc3854fbf6bd706fc203a9e9dfd8baf83f50
DELLXPS13JOSHUA\joshu <joshuagan9@gmail.com>
2025-01-23 12:23:12 +0800

    Converted tasks to new Task class

--- src/main/java/Yale.java
+++ src/main/java/Yale.java
@@ -5,7 +5,7 @@ public class Yale {
     private static final String NAME = "Yale";
     private static final String LINE = "\t____________________________________________________________";
 
-    private static final ArrayList<String> list = new ArrayList<>();
+    private static final ArrayList<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
         greet();
@@ -23,16 +23,32 @@ public class Yale {
         goodbye();
     }
 
+    private static class Task {
+        private final String name;
+        private boolean done = false;
+        public Task(String name) {
+            this.name = name;
+        }
+        public void setDone(boolean done) {
+            this.done = done;
+        }
+        public String toString() {
+            return "[" + (done ? "X" : " ") + "] " + name;
+        }
+    }
+
     private static void listOut() {
         System.out.println(LINE);
+        System.out.println("\tHere are the tasks in your list:");
         for (int i = 0; i < list.size(); i++) {
-            System.out.printf("\t%d. %s\n", i+1, list.get(i));
+            System.out.printf("\t%d.%s\n", i+1, list.get(i));
         }
         System.out.println(LINE);
     }
 
     private static void addToList(String msg) {
-        list.add(msg);
+        Task task = new Task(msg);
+        list.add(task);
         System.out.println(LINE);
         System.out.printf("\tadded: %s\n", msg);
         System.out.println(LINE);
