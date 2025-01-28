91302a5a30d78340a00d44e4a449694420f2b870
DELLXPS13JOSHUA\joshu <joshuagan9@gmail.com>
2025-01-28 14:26:35 +0800

    Add reading/writing of tasks from/to file Update ./gitignore to include tasks file

--- src/main/java/Yale.java
+++ src/main/java/Yale.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.PrintWriter;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -6,6 +9,7 @@ import java.util.regex.Pattern;
 public class Yale {
     private static final String NAME = "Yale";
     private static final String LINE = "\t____________________________________________________________";
+    private static final String TASKS_FILE = "tasks.txt";
 
     private static final Pattern MARK_REGEX = Pattern.compile("mark (\\d+)");
     private static final Pattern UNMARK_REGEX = Pattern.compile("unmark (\\d+)");
@@ -13,7 +17,6 @@ public class Yale {
     private static final Pattern DEADLINE_REGEX = Pattern.compile("deadline (.+) /by (.+)");
     private static final Pattern EVENT_REGEX = Pattern.compile("event (.+) /from (.+) /to (.+)");
     private static final Pattern DELETE_REGEX = Pattern.compile("delete (\\d+)");
-
 
     private static final String[][] COMMANDS = {
             {"mark", "mark [id]"},
@@ -24,7 +27,7 @@ public class Yale {
             {"delete", "delete [id]"}
     };
 
-    private static final ArrayList<Task> list = new ArrayList<>();
+    private static final ArrayList<Task> tasks = retrieveTasks();
 
     public static void main(String[] args) {
         greet();
@@ -56,30 +59,57 @@ public class Yale {
         goodbye();
     }
 
+    private static ArrayList<Task> retrieveTasks() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        try (Scanner in = new Scanner(new File(TASKS_FILE))) {
+            while (in.hasNextLine()) {
+                Task task = Task.fromCsv(in.nextLine());
+                if (task != null) {
+                    tasks.add(task);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        return tasks;
+    }
+
+    private static void writeTasksToFile() {
+        try (PrintWriter out = new PrintWriter(TASKS_FILE)) {
+            for (Task task : tasks) {
+                out.println(task.toCsv());
+            }
+            out.flush();
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+    }
+
     private static void deleteTask(int id) {
         System.out.println(LINE);
         if (checkInvalidID(id)) {
             System.out.println(LINE);
             return;
         }
-        Task task = list.remove(id-1);
+        Task task = tasks.remove(id-1);
+        writeTasksToFile();
         System.out.println("\tNoted. I've removed this task:");
         System.out.printf("\t  %s\n", task);
-        if (list.size() == 1) {
+        if (tasks.size() == 1) {
             System.out.println("\tNow you have 1 task in the list.");
         } else {
-            System.out.printf("\tNow you have %d tasks in the list.\n", list.size());
+            System.out.printf("\tNow you have %d tasks in the list.\n", tasks.size());
         }
         System.out.println(LINE);
     }
 
     private static boolean checkInvalidID(int id) {
-        if (list.isEmpty()) {
+        if (tasks.isEmpty()) {
             System.out.println("\tERROR: You don't have any tasks!");
             return true;
         }
-        if (id > list.size() || id <= 0) {
-            System.out.printf("\tERROR: The id should be from 1 to %d.\n", list.size());
+        if (id > tasks.size() || id <= 0) {
+            System.out.printf("\tERROR: The id should be from 1 to %d.\n", tasks.size());
             return true;
         }
         return false;
@@ -99,16 +129,29 @@ public class Yale {
     }
 
     private static abstract class Task {
-        private final String name;
-        private boolean done = false;
+        protected final String name;
+        protected boolean isDone = false;
         public Task(String name) {
             this.name = name;
         }
         public void setDone(boolean done) {
-            this.done = done;
+            this.isDone = done;
         }
         public String toString() {
-            return "[%s] %s".formatted(done ? "X" : " ", name);
+            return "[%s] %s".formatted(isDone ? "X" : " ", name);
+        }
+        protected abstract String[] getParams();
+        public String toCsv() {
+            return String.join("\0", getParams());
+        }
+        public static Task fromCsv(String csvString) {
+            String[] tokens = csvString.split("\0");
+            return switch (tokens.length) {
+                case 2 -> new ToDo(tokens[1]);
+                case 3 -> new Deadline(tokens[1], tokens[2]);
+                case 4 -> new Event(tokens[1], tokens[2], tokens[3]);
+                default -> null;
+            };
         }
     }
 
@@ -118,6 +161,9 @@ public class Yale {
         }
         public String toString() {
             return "[T]%s".formatted(super.toString());
+        }
+        protected String[] getParams() {
+            return new String[] {isDone ? "X" : " ", name};
         }
     }
 
@@ -129,6 +175,9 @@ public class Yale {
         }
         public String toString() {
             return "[D]%s (by: %s)".formatted(super.toString(), deadline);
+        }
+        protected String[] getParams() {
+            return new String[] {isDone ? "X" : " ", name, deadline};
         }
     }
 
@@ -143,6 +192,9 @@ public class Yale {
         public String toString() {
             return "[E]%s (from: %s, to: %s)".formatted(super.toString(), start, end);
         }
+        protected String[] getParams() {
+            return new String[]{isDone ? "X" : " ", name, start, end};
+        }
     }
 
     private static void markDone(int id, boolean done) {
@@ -151,8 +203,9 @@ public class Yale {
             System.out.println(LINE);
             return;
         }
-        Task task = list.get(id-1);
+        Task task = tasks.get(id-1);
         task.setDone(done);
+        writeTasksToFile();
         if (done) {
             System.out.println("\tNice! I've marked this task as done:");
         } else {
@@ -165,21 +218,22 @@ public class Yale {
     private static void listOut() {
         System.out.println(LINE);
         System.out.println("\tHere are the tasks in your list:");
-        for (int i = 0; i < list.size(); i++) {
-            System.out.printf("\t%d.%s\n", i+1, list.get(i));
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.printf("\t%d.%s\n", i+1, tasks.get(i));
         }
         System.out.println(LINE);
     }
 
     private static void addTask(Task task) {
-        list.add(task);
+        tasks.add(task);
+        writeTasksToFile();
         System.out.println(LINE);
         System.out.println("\tGot it. I've added this task:");
         System.out.printf("\t  %s\n", task);
-        if (list.size() == 1) {
+        if (tasks.size() == 1) {
             System.out.println("\tNow you have 1 task in the list.");
         } else {
-            System.out.printf("\tNow you have %d tasks in the list.\n", list.size());
+            System.out.printf("\tNow you have %d tasks in the list.\n", tasks.size());
         }
         System.out.println(LINE);
     }
