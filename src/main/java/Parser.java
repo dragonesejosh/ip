666710c58b08aa3194fe12a789656341b59d4eba
DELLXPS13JOSHUA\joshu <joshuagan9@gmail.com>
2025-01-31 17:16:14 +0800

    Add Command.java Move command checking and running to Command

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,22 +1,20 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Parser {
 
-    private static final Pattern MARK_REGEX = Pattern.compile("mark (\\d+)");
-    private static final Pattern UNMARK_REGEX = Pattern.compile("unmark (\\d+)");
-    private static final Pattern TODO_REGEX = Pattern.compile("todo (.+)");
-    private static final Pattern DEADLINE_REGEX = Pattern.compile("deadline (.+) /by (.+)");
-    private static final Pattern EVENT_REGEX = Pattern.compile("event (.+) /from (.+) /to (.+)");
-    private static final Pattern DELETE_REGEX = Pattern.compile("delete (\\d+)");
-
-    private static final String[][] COMMANDS = {
-            {"mark", "mark [id]"},
-            {"unmark", "unmark [id]"},
-            {"todo", "todo [name]"},
-            {"deadline", "deadline [name] /by [date]"},
-            {"event", "event [name] /from [start] /to [end]"},
-            {"delete", "delete [id]"}
+    private static final Command[] COMMANDS = {
+            new Command("mark", "mark [id]", "mark (\\d+)",
+                    (t, m) -> t.markDone(Integer.parseInt(m.group(1)), true)),
+            new Command("unmark", "unmark [id]", "unmark (\\d+)",
+                    (t, m) -> t.markDone(Integer.parseInt(m.group(1)), false)),
+            new Command("todo", "todo [name]", "todo (.+)",
+                    (t, m) -> t.addTask(new Task.ToDo(m.group(1)))),
+            new Command("deadline", "deadline [name] /by [date]",
+                    "deadline (.+) /by (.+)",
+                    (t, m) -> t.addTask(new Task.Deadline(m.group(1), m.group(2)))),
+            new Command("event", "event [name] /from [start] /to [end]",
+                    "event (.+) /from (.+) /to (.+)",
+                    (t, m) -> t.addTask(new Task.Event(m.group(1), m.group(2), m.group(3)))),
+            new Command("delete", "delete [id]", "delete (\\d+)",
+                    (t, m) -> t.deleteTask(Integer.parseInt(m.group(1))))
     };
 
     private final Ui ui;
@@ -30,38 +28,26 @@ public class Parser {
     }
 
     public boolean parseMsg(String msg) {
-        Matcher m;
-        boolean needWriteTasks = false;
         if (msg.equals("bye")) {
             return false;
         } else if (msg.equals("list")) {
             taskList.listOut();
-        } else if ((m = MARK_REGEX.matcher(msg)).matches()) {
-            needWriteTasks = taskList.markDone(Integer.parseInt(m.group(1)), true);
-        } else if ((m = UNMARK_REGEX.matcher(msg)).matches()) {
-            needWriteTasks = taskList.markDone(Integer.parseInt(m.group(1)), false);
-        } else if ((m = TODO_REGEX.matcher(msg)).matches()) {
-            needWriteTasks = taskList.addTask(new Task.ToDo(m.group(1)));
-        } else if ((m = DEADLINE_REGEX.matcher(msg)).matches()) {
-            needWriteTasks = taskList.addTask(new Task.Deadline(m.group(1), m.group(2)));
-        } else if ((m = EVENT_REGEX.matcher(msg)).matches()) {
-            needWriteTasks = taskList.addTask(new Task.Event(m.group(1), m.group(2), m.group(3)));
-        } else if ((m = DELETE_REGEX.matcher(msg)).matches()) {
-            needWriteTasks = taskList.deleteTask(Integer.parseInt(m.group(1)));
-        } else {
-            tryFindCommand(msg);
+            return false;
         }
-        if (needWriteTasks) {
-            storage.writeTasks(taskList.getTasks());
+        for (Command cmd : COMMANDS) {
+            if (cmd.tryCommand(taskList, storage, msg)) {
+                storage.writeTasks(taskList.getTasks());
+                return true;
+            }
         }
+        tryFindCommand(msg);
         return true;
     }
 
     private void tryFindCommand(String msg) {
         ui.beginOutput();
-        for (String[] command : COMMANDS) {
-            if (msg.startsWith(command[0])) {
-                ui.printError("The proper format for %s is '%s'.", command[0].toUpperCase(), command[1]);
+        for (Command cmd : COMMANDS) {
+            if (cmd.partialMatch(ui, msg)) {
                 ui.endOutput();
                 return;
             }
