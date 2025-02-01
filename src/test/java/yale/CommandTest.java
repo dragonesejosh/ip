c5f77005bc9204a83882860921edfac44b31d59f
DELLXPS13JOSHUA\joshu <joshuagan9@gmail.com>
2025-02-01 16:35:41 +0800

    Add JUnit tests for Command and Parser Add dummy instances for several classes to use in testing

---
+++ src/test/java/yale/CommandTest.java
@@ -0,0 +1,48 @@
+package yale;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class CommandTest {
+    @Test
+    public void regexTest() {
+        String[] output = {""};
+        Command command = new Command("", "",
+                "dummy (.+) /next (\\d+)",
+                (t, m) -> {
+                    output[0] = m.group(1) + "|" + m.group(2);
+                    return false;
+                });
+
+        command.tryCommand(null, null,
+                "dummy hello there /next 1234");
+        assertEquals("hello there|1234", output[0]);
+
+        output[0] = "";
+        command.tryCommand(null, null,
+                "dummy goodbye //next 1234");
+        assertEquals("", output[0]);
+
+        output[0] = "";
+        command.tryCommand(null, null,
+                "dummy greetings /next 12d34");
+        assertEquals("", output[0]);
+    }
+
+    @Test
+    public void partialMatchTest() {
+        Command command = new Command("dummy", "", "",
+                (t, m) -> false);
+
+        assertTrue(command.partialMatch(Dummy.DUMMY_UI, "dummy complete /next 1234"));
+
+        assertTrue(command.partialMatch(Dummy.DUMMY_UI, "dummy incomplete"));
+
+        assertTrue(command.partialMatch(Dummy.DUMMY_UI, "dummycombine"));
+
+        assertFalse(command.partialMatch(Dummy.DUMMY_UI, "dumm"));
+    }
+}
